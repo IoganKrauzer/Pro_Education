b9bb72ea16824f3e31081f01a0af5d0d23714034
Sergey <asmcm88@gmail.com>
2023-04-20 21:43:22 +0300

    T00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+//Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int square = number * number;
+Console.Write("Ответ: ");
+Console.WriteLine(square);
