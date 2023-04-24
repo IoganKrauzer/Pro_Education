d280eb62c1e8c7c2b4c0358a43433dd310f0bd75
Sergey <asmcm88@gmail.com>
2023-04-24 21:30:47 +0300

    seminar's new tasks

---
+++ Task09/Program.cs
@@ -0,0 +1,39 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Console.Clear();
+int n = new Random().Next(10, 100);
+Console.WriteLine(n);
+// Console.WriteLine("Случайное число из отрезка 10 ... 99 : " + n);
+// int a = n / 10;
+// int b = n % 10;
+
+// Console.WriteLine(a);
+// Console.WriteLine(b);
+
+// if (a > b)
+// {
+//     Console.WriteLine(a);
+// }
+// else
+// {
+//     Console.WriteLine(b);
+// }
+
+// int max = a > b ? a : b; 
+// Console.WriteLine(max);
+
+int max = MaxDigit(n);
+Console.WriteLine(max);
+
+int MaxDigit(int num)
+{
+    int a = num / 10;
+    int b = num % 10;
+    int max = a > b ? a : b; 
+    return max;
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,23 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+// 12 мин
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Случайное число в отрезке от 10 ... 90 " + number);
+
+
+int answer = Digit(number);
+Console.WriteLine(answer);
+
+int Digit(int num)
+{
+    int firstNum = num / 100;
+    int thirdNum = num % 10;
+    int mathDecision = firstNum * 10 + thirdNum;
+    return mathDecision;
+}
