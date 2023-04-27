f84f32f1486981f239eaed308086366709fc21c3
Sergey <asmcm88@gmail.com>
2023-04-28 00:35:10 +0300

    HW 19

---
+++ Task19/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+
+int revNum = ReversNumb(num);
+string answer = (num == revNum && num != 0) ? $"Число: \" {num} \" является полидромом!" : $"Число: \" {num} \" не является полидромом!";
+Console.WriteLine(answer);
+
+
+int ReversNumb(int n)
+{
+    int reverse = 0;
+    if (n <= -10 || n >= 10)
+    {
+        while (n != 0)
+        {
+            int remain = n % 10;
+            reverse = reverse * 10 + remain;
+            n = n / 10;
+        }
+        return (reverse);
+    }
+    return 0;
+}
